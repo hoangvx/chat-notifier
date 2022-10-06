aadbab6a85f12820701e7d2991a2aebbdd990866
hoang.vx <hoang.vx@glv.co.jp>
2022-10-06 10:34:57 +0900

    init1

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in chat-notifier.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/chat/notifier.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "notifier/version"
+
+module Chat
+  module Notifier
+    class Error < StandardError; end
+    # Your code goes here...
+
+  end
+end
